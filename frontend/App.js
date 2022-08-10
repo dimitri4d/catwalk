bbbf6f9f70e98be2a5a62958cac8df5846705b16
Gary Poon Tip <gary.tip@intelliware.com>
2022-08-09 22:02:09 -0400

    front end

--- frontend/App.js
+++ frontend/App.js
@@ -1,68 +1,115 @@
-import 'regenerator-runtime/runtime';
-import React from 'react';
+import "regenerator-runtime/runtime";
+import React from "react";
 
-import './assets/global.css';
+import "./assets/global.css";
 
-import { getGreetingFromContract, setGreetingOnContract } from './near-api';
-import { EducationalText, SignInPrompt, SignOutButton } from './ui-components';
+import { setTransaction } from "./near-api";
+import { SignInPrompt, SignOutButton } from "./ui-components";
 
+const BOATLOAD_OF_GAS = 300000000000000;
+const BLOCK_FREQUENCY = 1;
 
 export default function App() {
-  const [valueFromBlockchain, setValueFromBlockchain] = React.useState();
+  const [formValues, setFormValues] = React.useState({
+    amount: 0,
+    frequency: 0,
+    address: "",
+  });
 
-  const [uiPleaseWait, setUiPleaseWait] = React.useState(true);
+  const isValidForm = React.useMemo(() => {
+    if (
+      formValues.amount === 0 ||
+      formValues.frequency === 0 ||
+      formValues.address === ""
+    )
+      return false;
+    return true;
+  }, [formValues]);
 
-  // Get blockchian state once on component load
-  React.useEffect(() => {
-    getGreetingFromContract()
-      .then(setValueFromBlockchain)
-      .catch(alert)
-      .finally(() => {
-        setUiPleaseWait(false);
-      });
-  }, []);
+  const isSignedIn = window.walletConnection.isSignedIn();
 
-  /// If user not signed-in with wallet - show prompt
-  if (!window.walletConnection.isSignedIn()) {
-    // Sign-in flow will reload the page later
-    return <SignInPrompt greeting={valueFromBlockchain}/>;
-  }
-
-  function changeGreeting(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    setUiPleaseWait(true);
-    const { greetingInput } = e.target.elements;
-    setGreetingOnContract(greetingInput.value)
-      .then(getGreetingFromContract)
-      .then(setValueFromBlockchain)
-      .catch(alert)
-      .finally(() => {
-        setUiPleaseWait(false);
-      });
+    if (isValidForm) {
+      setTransaction(
+        formValues.amount,
+        BOATLOAD_OF_GAS,
+        formValues.frequency,
+        formValues.address
+      );
+    }
   }
 
   return (
     <>
-      <SignOutButton accountId={window.accountId}/>
-      <main className={uiPleaseWait ? 'please-wait' : ''}>
-        <h1>
-          The contract says: <span className="greeting">{valueFromBlockchain}</span>
-        </h1>
-        <form onSubmit={changeGreeting} className="change">
-          <label>Change greeting:</label>
-          <div>
+      {isSignedIn && <SignOutButton accountId={window.accountId} />}
+      <main>
+        <h1>Catwalk</h1>
+        <form onSubmit={handleSubmit} className="change">
+          <label htmlFor="amountInput">Amount:</label>
+          <div style={{ marginBottom: "10px" }}>
+            <input
+              type="number"
+              id="amountInput"
+              value={formValues.amount}
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  amount: e.target.value ? parseInt(e.target.value) : 0,
+                })
+              }
+            />
+          </div>
+          <label htmlFor="frequencyInput">Frequency:</label>
+          <div style={{ marginBottom: "10px" }}>
+            <select
+              style={{
+                width: "100%",
+                backgroundColor: "#444",
+                color: "#fff",
+                padding: "5px",
+                fontSize: "18px",
+                border: "none",
+              }}
+              type="number"
+              id="frequencyInput"
+              value={formValues.frequency}
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  frequency: e.target.value ? parseInt(e.target.value) : 0,
+                })
+              }
+            >
+              <option value={BLOCK_FREQUENCY}>Daily</option>
+              <option value={BLOCK_FREQUENCY}>Weekly</option>
+              <option value={BLOCK_FREQUENCY}>Monthly</option>
+              <option value={BLOCK_FREQUENCY}>Annually</option>
+            </select>
+          </div>
+          <label htmlFor="addressInput">Deposit Address:</label>
+          <div style={{ marginBottom: "10px" }}>
             <input
               autoComplete="off"
-              defaultValue={valueFromBlockchain}
-              id="greetingInput"
+              id="addressInput"
+              value={formValues.address}
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  address: e.target.value ? e.target.value : "",
+                })
+              }
             />
+          </div>
+          {isSignedIn ? (
             <button>
-              <span>Save</span>
+              <span>Submit</span>
               <div className="loader"></div>
             </button>
-          </div>
+          ) : (
+            <SignInPrompt />
+          )}
         </form>
-        <EducationalText/>
       </main>
     </>
   );
